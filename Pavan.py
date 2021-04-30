018ad1f0a474dce5122be5dc4b9374135c46fdc0
Pavan725 <83358574+Pavan725@users.noreply.github.com>
2021-04-30 18:56:18 +0530

    Update Pavan.py

--- Pavan.py
+++ Pavan.py
@@ -1,4 +1,4 @@
-@@ -1,5 +1,5 @@
+
 def most_frequent():
     str=input('enter a string')
 def most_frequent(str):
@@ -6,10 +6,23 @@ def most_frequent(str):
     d=dict()
     for i in str:
         if i in d:
-@@ -10,4 +10,4 @@ def most_frequent():
+def most_frequent():
     for r in d_sort:
         print(r,'=',d[r])
 
 most_frequent()
-most_frequent(input("enter the string")) 
-0 comments on commit 1d0d04e
+most_frequent(input("enter the string
+
+  
+
+
+    
+    
+
+
+
+
+
+
+
+
